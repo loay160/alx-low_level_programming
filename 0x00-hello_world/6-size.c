4629d8d6b9fdb5a1fa3ab5cd7693d11db57f2639
loay ashraf <117324979+loay160@users.noreply.github.com>
2023-07-13 22:18:04 +0000

    MY commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main -Entry point
  *
- * description: using sizeof to print the size of various types
+ * Description: using sizeof to print the size of various types
  *
  * Return: Alawys 0 (Success)
 */
