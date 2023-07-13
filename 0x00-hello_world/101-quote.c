1370dfbc8df955b1767746e9a834d1873778bdb6
loay ashraf <117324979+loay160@users.noreply.github.com>
2023-07-13 22:41:26 +0000

    MY commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,14 @@
  * main - Entry point
  *
  * Description: print a qoute using write function
- *	ssize_t rite(int fd.const void *buf.size_t count);
+ *	ssize_t write(int fd. const void *buf. size_t count);
  *
  * Return: 1 (error).
 */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
-	write(1. quo. 59);
+	write(1, quo, 59);
 	return (1);
 }
