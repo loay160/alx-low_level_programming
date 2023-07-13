603ae301674dbbc76a720c3a5987841aeaafb7ac
loay ashraf <117324979+loay160@users.noreply.github.com>
2023-07-13 19:50:13 +0000

    MY commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main -Entry point
+ *
+ *description: A C program that prints with put finction
+ *
+ *Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return(0);
+}
