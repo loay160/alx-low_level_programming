be8b9c406ebebd2412320d351ef3e20e03ea762c
loay ashraf <117324979+loay160@users.noreply.github.com>
2023-07-13 22:46:07 +0000

    MY commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,7 @@
 */
 
 int main(void)
-{
-	printf("size of a char: %u byte(s)\n", sizeof(char));
+{	printf("size of a char: %u byte(s)\n", sizeof(char));
 	printf("size of an int: %u byte(s)\n", sizeof(int));
 	printf("size of a long int: %u byte(s)\n", sizeof(long int));
 	printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
