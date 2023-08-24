4d5ddfeaec7a8702d8c2d67a69efb31ea4363a7e
loay ashraf <117324979+loay160@users.noreply.github.com>
2023-08-25 01:55:25 +0300

    Cc

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -3,17 +3,16 @@
 
 /**
  * free_list - frees a linked list
+ *
  * @head: list_t list to be freed
  */
-void free_list(list_t *head)
-{
-	list_t *temp;
 
-	while (head)
-	{
+void free_list(list_t *head)	{
+	list_t *temp;
+	while (head)	{
 		temp = head->next;
 		free(head->str);
 		free(head);
 		head = temp;
+		}
 	}
-}
