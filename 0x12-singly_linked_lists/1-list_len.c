9776ca32664922a2fd5fe089a8d313a3a90868b9
loay ashraf <117324979+loay160@users.noreply.github.com>
2023-08-25 01:45:02 +0300

    Cc

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,16 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * list_len - returns the number of elements in a linked
+ *
+ * @h: pointer to the list_t 
  *
  * Return: number of elements in h
  */
-size_t list_len(const list_t *h)
-{
-	size_t n = 0;
 
-	while (h)
-	{
+size_t list_len(const list_t *h) {	
+	size_t n = 0; while (h) {
 		n++;
-		h = h->next;
-	}
+		h = h->next; }
 	return (n);
 }
